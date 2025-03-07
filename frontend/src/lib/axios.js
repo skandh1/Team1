5977f47acd49ec32a1b346fed814d9f8065ff6b7
Skandh <Skandhjadon02@gmail.com>
2025-03-07 16:30:50 +0530

    added url for backedn

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v1" : "/api/v1",
-	withCredentials: true,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api/v1"
+      : "https://teamify-sigma.vercel.app/api/v1",
+  withCredentials: true,
 });
