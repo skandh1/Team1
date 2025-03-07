d81098cd441660f3d597e8a5c2e46abba47b5178
Skandh <Skandhjadon02@gmail.com>
2025-03-07 16:42:30 +0530

    hosting effort

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api/v1"
-      : "https://teamify-sigma.vercel.app/api/v1",
+      : "/api/v1",
   withCredentials: true,
 });
