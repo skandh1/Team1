8a5660566ae714c7f0901c34ee7df93be3ce844c
Skandh <Skandhjadon02@gmail.com>
2025-03-08 11:09:34 +0530

    some changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api/v1"
-      : `${import.meta.evn.URL}/api/v1`,
+      : `${import.meta.env.URL}/api/v1`,
   withCredentials: true,
 });
