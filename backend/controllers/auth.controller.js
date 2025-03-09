73391fac8cf1416068f30035700866ce2f8fadef
Skandh <Skandhjadon02@gmail.com>
2025-03-09 20:40:33 +0530

    e25

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -45,7 +45,7 @@ export const signup = async (req, res) => {
     res.cookie("jwt-linkedin", token, {
       httpOnly: true, // prevent XSS attack
       maxAge: 3 * 24 * 60 * 60 * 1000,
-      // sameSite: "None", // prevent CSRF attacks,
+      sameSite: "None", // prevent CSRF attacks,
       secure: process.env.NODE_ENV === "production", // prevents man-in-the-middle attacks
     });
 
@@ -87,7 +87,7 @@ export const login = async (req, res) => {
     await res.cookie("jwt-linkedin", token, {
       httpOnly: true,
       maxAge: 3 * 24 * 60 * 60 * 1000,
-      // sameSite: "none",
+      sameSite: "None",
       secure: process.env.NODE_ENV === "production",
     });
 
