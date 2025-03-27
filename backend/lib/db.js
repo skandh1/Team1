1a2a09cfc422b9847f6690d8a5945b369c6d06a3
Skandh <Skandhjadon02@gmail.com>
2025-03-27 10:21:28 +0530

    e30

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://shuskason:super321@cluster0.5d70o.mongodb.net/linkedin_db?");
+		const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb+srv://shuskason:super321@cluster0.5d70o.mongodb.net/teamfiy_db?");
 		console.log(`MongoDB connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.error(`Error connecting to MongoDB: ${error.message}`);
--- backend/routes/chat.route.js
+++ backend/routes/chat.route.js
@@ -7,10 +7,9 @@ import searchUser from '../controllers/serach.controller.js';
 const router = express.Router();
 
 router.get("/token", protectRoute, getChatToken)
-// router.post("/chat", protectRoute, createOrUpdateChat)
 router.get("/search", protectRoute, searchUser)
 router.post("/channel", protectRoute, channel)
 router.get("/users", protectRoute, getUser)
-// router.post("/create", protectRoute, createChat)
+
 
 export default router;
